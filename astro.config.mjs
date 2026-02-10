3a526319962fb859aaeca37adeeface72094c984
huangjin-bit <1119268275@qq.com>
2026-02-11 05:22:40 +0800

    fix: remove sitemap plugin to fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://huangjin-bit.github.io',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx()],
   output: 'static',
   markdown: {
     shikiConfig: {
